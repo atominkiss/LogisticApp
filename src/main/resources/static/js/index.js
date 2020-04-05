99c35eb98ff7de9be4f7fc32797441c672e1fd4e
atomink <atomink@gmail.com>
2020-04-05 16:29:59 +0300

    Initial commit

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,18 @@
+webix.ready(function(){
+    webix.ui({
+        type:"line",
+        container:"app",
+        id: "root",
+        height: 400,
+        rows:[
+            { template:"Row 1" },
+            { template:"Row 2" },
+            {
+                cols:[
+                    {template: "col 1"},
+                    {template: "col 2"},
+                ]
+            }
+        ]
+    })
+});
---
+++ src/test/java/net/protoprint/logisticapp/LogisticappApplicationTests.java
@@ -0,0 +1,13 @@
+package net.protoprint.logisticapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
